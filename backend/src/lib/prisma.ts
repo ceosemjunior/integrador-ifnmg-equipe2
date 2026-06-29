d8ea5d176f97ed41a3f04562fcb9c6c7ad81d583
jamsnxs <jailsonhh72c@gmail.com>
2026-06-29 20:32:17 -0300

    chore: simplifica setup do Prisma

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from "@prisma/client";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { env } from "../env/index.js";
+import { PrismaClient } from "../../generated/prisma/client";
+import { env } from "../env/index";
 
 const adapter = new PrismaBetterSqlite3({
   url: env.DATABASE_URL,
 });
 
-export const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({ adapter });
 
-export default prisma;
+export { prisma };
